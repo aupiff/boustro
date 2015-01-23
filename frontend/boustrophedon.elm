b029fc43b1bb532da07e286ebcbbe99f8d672e50
Roy Blankman <riblankman@gmail.com>
2015-01-22 21:46:50 -0800

    starting to modularize code
    first steps to creating swipable interface

--- frontend/boustrophedon.elm
+++ frontend/boustrophedon.elm
@@ -16,17 +16,62 @@ serverUrl = "http://192.168.1.65:8000/"
 fileName : Signal String
 fileName = S.constant "jaures.txt"
 
-type alias AppState = String
+type alias AppState = { fullText     : String
+                      , currentPage  : Html
+                      , priorPages   : List Html
+                      , futurePages  : List Html
+                      }
 
-state : Signal AppState
-state = let req = S.map (\x -> Http.get (serverUrl ++ "texts/"  ++ x)) fileName
-            response = Http.send <| req
-            getContent : Http.Response String -> String
-            getContent response = case response of
-                Http.Success str -> str
-                Http.Waiting     -> "waiting."
-                Http.Failure _ _ -> "http request failed."
-        in S.map getContent response
+type UserInput = NextPage
+               | PrevPage
+               | SetText String
+
+userInput : Signal UserInput
+userInput = S.mergeMany [ S.map SetText textContent ]
+
+type alias InputData = (UserInput, ViewDimensions)
+
+stringToState : String -> ViewDimensions -> AppState
+stringToState str viewDims =
+    let nonEmptyLines : String -> List String
+        nonEmptyLines = filter (not << String.isEmpty) << String.lines
+        charPerLine = floor <| toFloat viewDims.textWidth / 7.25
+        txtLines : List Html
+        txtLines = snd << foldr boustrophedon (True, [])
+                       << L.map String.fromList << toParLines charPerLine
+                       << L.concatMap (paragraphPrefix << String.toList)
+                       <| nonEmptyLines str
+    in  { fullText    = str
+        , currentPage = L.head txtLines
+        , priorPages  = []
+        , futurePages = L.tail txtLines
+        }
+
+nextState : InputData -> AppState -> AppState
+nextState (userInput, viewDimensions) pState =
+    case userInput of
+        SetText str -> stringToState str viewDimensions
+        NextPage    -> pState
+        PrevPage    -> pState
+
+emptyState = { fullText     = "empty"
+             , currentPage  = (text "empty")
+             , priorPages   = []
+             , futurePages  = []
+             }
+
+appState : Signal AppState
+appState = S.foldp nextState emptyState (S.map2 (,) userInput currentViewDimensions)
+
+textContent : Signal String
+textContent = let req = S.map (\x -> Http.get (serverUrl ++ "texts/"  ++ x)) fileName
+                  response = Http.send <| req
+                  getContent : Http.Response String -> String
+                  getContent response = case response of
+                      Http.Success str -> str
+                      Http.Waiting     -> "waiting."
+                      Http.Failure _ _ -> "http request failed."
+              in S.map getContent response
 
 boustrophedon : String -> RenderState -> RenderState
 boustrophedon str (reverseState, elList) =
@@ -39,11 +84,15 @@ boustrophedon str (reverseState, elList) =
 
 type alias ViewDimensions = { fullContainerWidth : Int
                             , fullContainerHeight : Int
+                            , textWidth : Int
+                            , textHeight : Int
                             }
 
 viewHelper : (Int, Int) -> ViewDimensions
 viewHelper (w, h) = { fullContainerWidth = w
                     , fullContainerHeight = h
+                    , textWidth = min (w - 40) 660
+                    , textHeight = h - 10
                     }
 
 currentViewDimensions : Signal ViewDimensions
@@ -61,28 +110,16 @@ paragraphPrefix str = ('¶' :: ' ' :: str) ++ [' ', ' ']
 
 main : Signal Element
 main = scene <~ currentViewDimensions
-              ~ state
+              ~ appState
 
 scene : ViewDimensions -> AppState -> Element
-scene viewDims content =
-    let nonEmptyLines : String -> List String
-        nonEmptyLines = filter (not << String.isEmpty) << String.lines
-        textWidth = min (viewDims.fullContainerWidth - 40) 660
-        charPerLine = floor <| toFloat textWidth / 8.5
-        txtLines : List String -> List Html
-        txtLines = snd << foldr boustrophedon (False, [])
-                       << L.map String.fromList << toParLines charPerLine
-                       << L.concatMap (paragraphPrefix << String.toList)
-        getLineElements = txtLines << nonEmptyLines
+scene viewDims appState =
+    let renderTextView = toElement viewDims.textWidth viewDims.textHeight
+        containerDivProps = style [ ("width", (toString viewDims.textWidth) ++ "px")
+                                  , ("margin", "0 auto") ]
+        textView : List Html -> Html
+        textView = div [containerDivProps]
         fullContainer = container viewDims.fullContainerWidth
                                   viewDims.fullContainerHeight
                                   middle
-        renderTextView = toElement textWidth
-                                   (viewDims.fullContainerHeight - 10)
-        containerDivProps = style [ ("width", (toString textWidth) ++ "px")
-                                  , ("margin", "0 auto")
-                                  ]
-        textView : List Html -> Html
-        textView = div [containerDivProps]
-        textViewEl = renderTextView << textView <| getLineElements content
-    in  fullContainer textViewEl
+    in  fullContainer << renderTextView << textView <| [ appState.currentPage ]
