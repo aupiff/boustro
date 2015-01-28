0581790a3ef10237001b754ce38508509a7bb66b
Roy Blankman <riblankman@gmail.com>
2015-01-28 15:15:32 -0800

    standalone seems to work

--- frontend/Boustrophedon.elm
+++ frontend/Boustrophedon.elm
@@ -67,7 +67,7 @@ nextState userInput pState =
                      }
         Swipe NoSwipe -> pState
 
-emptyState = stringToState default_intro_text <| viewHelper (600, 300)
+emptyState = stringToState Server.default_text <| viewHelper (600, 300)
 
 appState : Signal AppState
 appState = S.foldp nextState emptyState userInput
@@ -89,23 +89,3 @@ scene appState =
 
 main : Signal Element
 main = scene <~ appState
-
-default_intro_text ="""
-The βουστροφηδόν Manifesto
-
-January 30th, 2015
-
-Dear reader, how do you your eyes feel? Are they fatigued from the hellish gymnastics required during reading? Have they skipped from the right margin, across the entire block of text, to the left one line too many? Are your eyes desperately searching for the next appropriate line without the slightest visual cue? Let boustrophedon put you at ease--there are no gynastics here, just a comfortable winding stroll through the lines of great literature. Double tap the right side of the screen to advance a page, or double tap the left side of the screen to go back a page.
-
-Boustro etymology. comes from the greek works bous and strophe.
-
-Reading this strange mirrored text is difficult you say? Well, sure, at first... but I'm sure Tony Hawk had difficulty riding switch for the first time, too. Later on, being able to skate with either foot forward became essential for transition skating... Everyone reading today is like a skateboarder who never learned to push with their other foot--we've been cheated of fun and comfort. We are the sorriest oxen in the world, forced to needlessly do twice as much work as necessary.
-
-Step onto the half-pipe of life. Flip turn on your next lap.
-
-Tolstoy reads fine in boustro: Господин этот во все время путешествия старательно избегал общения и знакомства с пассажирами. На заговариванья соседей он отвечал коротко и резко и или читал, или, глядя в окно, курил, или, достав провизию из своего старого мешка, пил чай, или закусывал.
-
-Plato, too, is a delight to read when we take it easy on our eyes: Σωκράτης κατέβην χθὲς εἰς Πειραιᾶ μετὰ Γλαύκωνος τοῦ Ἀρίστωνος προσευξόμενός τε τῇ θεῷ καὶ ἅμα τὴν ἑορτὴν βουλόμενος θεάσασθαι τίνα τρόπον ποιήσουσιν ἅτε νῦν πρῶτον ἄγοντες. καλὴ μὲν οὖν μοι καὶ ἡ τῶν ἐπιχωρίων πομπὴ ἔδοξεν εἶναι, οὐ μέντοι ἧττον ἐφαίνετο πρέπειν ἣν οἱ Θρᾷκες ἔπεμπον.
-
-So we march on, oxen against the plow, bourne ceaselessly along the eterenal es.
-"""
