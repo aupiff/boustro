c8c220237fcf0338e44f9bfb59dce67af39d8d5e
Roy Blankman <riblankman@gmail.com>
2015-01-31 00:24:05 -0800

    cleaning up update function

--- frontend/Boustrophedon.elm
+++ frontend/Boustrophedon.elm
@@ -11,8 +11,7 @@ import Typography
 
 stringToState : String -> ModelState
 stringToState str = { fullText  = Typography.strToWordArray str
-                    , wordIndex = 0
-                    }
+                    , wordIndex = 0 }
 
 viewFromModelAndDims : ModelState -> ViewDimensions -> ViewState
 viewFromModelAndDims modelState viewDimensions =
@@ -22,46 +21,42 @@ viewFromModelAndDims modelState viewDimensions =
        , view = view
        , viewDimensions = viewDimensions }
 
-update : (UserInput, WindowDimensions) -> (ModelState, ViewState) -> (ModelState, ViewState)
-update (userInput, (w, h)) (modelState, viewState) =
-    if | ((viewState.viewDimensions.fullWidth, viewState.viewDimensions.fullHeight) /= (w, h)) ->
-            let newViewDimensions = viewHelper (w, h)
-                a = log "dims" (w, h)
-                newViewState = viewFromModelAndDims modelState newViewDimensions
-            in  (modelState, newViewState)
-       | otherwise -> case userInput of
-            SetText str ->
-                let newModelState = stringToState str
-                    newViewState = viewFromModelAndDims newModelState viewState.viewDimensions
-                in (newModelState, newViewState)
-            Swipe Next ->
-                let idx = modelState.wordIndex + viewState.pageWordCount
-                    newModelState = { modelState | wordIndex <- idx }
-                    newViewState = viewFromModelAndDims newModelState viewState.viewDimensions
-                in (newModelState, newViewState)
-            Swipe Prev ->
-                let (page, wc) = Typography.typesetPrevPage modelState viewState.viewDimensions
-                    newModelState = { modelState | wordIndex <- modelState.wordIndex - wc }
-                    view = scene page viewState.viewDimensions
-                    newViewState = { viewState | pageWordCount <- wc
-                                               , view <- view }
-                in (newModelState, newViewState)
-            Swipe NoSwipe -> (modelState, viewState)
+update : Update -> (ModelState, ViewState) -> (ModelState, ViewState)
+update update (modelState, viewState) =
+    case update of
+        ViewChange viewDims -> (modelState,
+                                viewFromModelAndDims modelState viewDims)
+        Input (SetText str) ->
+                  let newModelState = stringToState str
+                      newViewState = viewFromModelAndDims newModelState viewState.viewDimensions
+                  in (newModelState, newViewState)
+        Input (Swipe Next) ->
+                  let idx = modelState.wordIndex + viewState.pageWordCount
+                      newModelState = { modelState | wordIndex <- idx }
+                      newViewState = viewFromModelAndDims newModelState viewState.viewDimensions
+                  in (newModelState, newViewState)
+        Input (Swipe Prev) ->
+                  let (page, wc) = Typography.typesetPrevPage modelState viewState.viewDimensions
+                      newModelState = { modelState | wordIndex <- modelState.wordIndex - wc }
+                      view = scene page viewState.viewDimensions
+                      newViewState = { viewState | pageWordCount <- wc
+                                                 , view <- view }
+                  in (newModelState, newViewState)
+        Input (Swipe NoSwipe) -> (modelState, viewState)
 
 emptyState = ( stringToState Server.defaultText
              , { pageWordCount = 0
                , view = empty
-               , viewDimensions = viewHelper (300, 300)
-               }
-             )
+               , viewDimensions = viewHelper (300, 300) } )
 
 appState : Signal (ModelState, ViewState)
-appState = S.foldp update emptyState <| S.map2 (,) userInput currentWindowDimensions
+appState = let input = (S.map Input userInput)
+               viewChange = (S.map ViewChange currentViewDimensions)
+           in S.foldp update emptyState <| S.merge input viewChange
 
 userInput : Signal UserInput
 userInput = S.mergeMany [ S.map SetText Server.textContent
-                        , S.map Swipe swipe
-                        ]
+                        , S.map Swipe swipe ]
 
 main : Signal Element
 main = S.map (.view << snd) appState
