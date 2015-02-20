aad0bddff756e627bdc59dda0004a6f3867a2381
Roy Blankman <riblankman@gmail.com>
2015-02-20 10:55:23 -0800

    making progress with the selection menu

--- frontend/Menu.elm
+++ frontend/Menu.elm
@@ -3,18 +3,21 @@ import Text (plainText)
 import Graphics.Element (Element)
 import Signal as S
 import Json.Decode (..)
+import Result (Result(Ok, Err))
 
+textListDecoder : Decoder (List (String, String))
 textListDecoder = list textPartDecoder
 
+textPartDecoder : Decoder (String, String)
 textPartDecoder = object2 (,) ("title" := string) ("path" := string)
 
-formatTextList : Maybe String -> String
+formatTextList : Maybe String -> Result String (List (String, String))
 formatTextList textListMaybe = case textListMaybe of
-    Nothing -> "Nothing"
-    Just json -> let result = decodeString textListDecoder json
-                 in case result of
-                     Ok val -> toString val
-                     otherwise -> "Json parsing failed"
+    Nothing -> Err "Nothing"
+    Just json -> decodeString textListDecoder json
+
+generateMenu : Result String (List (String, String)) -> Element
+generateMenu = plainText << toString
 
 main : Signal Element
-main = S.map (plainText << formatTextList) Server.textList
+main = S.map (generateMenu << formatTextList) Server.textList
