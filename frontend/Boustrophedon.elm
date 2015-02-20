e1e23db5b9e9c92a2c2676f510c2a8f1a6019ee3
Roy Blankman <riblankman@gmail.com>
2015-02-20 02:59:00 -0800

    fixing paging bug

--- frontend/Boustrophedon.elm
+++ frontend/Boustrophedon.elm
@@ -6,6 +6,7 @@ import UI (..)
 import Model (..)
 import Utils
 import Typography
+import Debug (log)
 
 stringToModelState : String -> ViewDimensions -> ModelState
 stringToModelState str viewDimensions =
@@ -13,6 +14,7 @@ stringToModelState str viewDimensions =
         wordIndex = 0
         (page, wc) = Typography.typesetPage text wordIndex viewDimensions
         view = textScene page viewDimensions
+        _ = log "wc" <| toString wc
     in TextModel { fullText  = text
                  , wordIndex = wordIndex
                  , pageWordCount = wc
@@ -45,33 +47,34 @@ updateState update (viewDimensions, modelState) =
             let newModelState = stringToModelState str viewDimensions
             in (viewDimensions, newModelState)
         Input (Gesture Next) ->
-            case modelState of
-                TextModel textModel ->
-                    let idx = textModel.wordIndex + textModel.pageWordCount
-                    in if | idx < (Array.length textModel.fullText) ->
-                            let (page, wc) = Typography.typesetPage textModel.fullText idx viewDimensions
-                                view = textScene page viewDimensions
-                            in ( viewDimensions
-                               , TextModel { textModel | wordIndex <- idx
-                                                       , view <- view
-                                                       , pageWordCount <- wc
-                                           })
-                          | otherwise -> (viewDimensions, modelState)
-                otherwise -> (viewDimensions, modelState)
+            let newModel = case modelState of
+                    TextModel textModel ->
+                        let textLength = Array.length textModel.fullText
+                            newWordIndex = textModel.wordIndex + textModel.pageWordCount
+                            idx = if newWordIndex < textLength then newWordIndex else textModel.wordIndex
+                            (page, wc) = Typography.typesetPage textModel.fullText idx viewDimensions
+                            view = textScene page viewDimensions
+                            _ = log "new word index" <| toString idx
+                            _ = log "old word index" <| toString textModel.wordIndex
+                            _ = log "word count" <| toString textModel.pageWordCount
+                        in TextModel { textModel | wordIndex <- idx
+                                                 , pageWordCount <- wc
+                                                 , view <- view }
+                    otherwise -> modelState
+            in (viewDimensions, newModel)
         Input (Gesture NoGesture) -> (viewDimensions, modelState)
         Input (Gesture Prev) ->
-            case modelState of
-                TextModel textModel ->
-                    let pwc = Typography.prevPageWordCount textModel.fullText idx viewDimensions
-                        idx = max (textModel.wordIndex - pwc) 0
-                        (page, wc) = Typography.typesetPage textModel.fullText idx viewDimensions
-                        view = textScene page viewDimensions
-                    in ( viewDimensions
-                       , TextModel { textModel | wordIndex <- idx
-                                               , view <- view
-                                               , pageWordCount <- wc
-                                   })
-                otherwise -> (viewDimensions, modelState)
+            let newModel = case modelState of
+                    TextModel textModel ->
+                        let pwc = Typography.prevPageWordCount textModel.fullText textModel.wordIndex viewDimensions
+                            idx = max (textModel.wordIndex - pwc) 0
+                            (page, wc) = Typography.typesetPage textModel.fullText idx viewDimensions
+                            view = textScene page viewDimensions
+                        in TextModel { textModel | wordIndex <- idx
+                                                 , pageWordCount <- wc
+                                                 , view <- view }
+                    otherwise -> modelState
+            in (viewDimensions, newModel)
 
 main : Signal Element
 main = S.map (modelToView << snd) appState
