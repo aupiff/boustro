1a0d64bec024fddfaa65609b842bf386dc0fb217
Roy Blankman <riblankman@gmail.com>
2015-06-28 00:30:13 -0700

    moving to servant backend

--- backend/Main.hs
+++ backend/Main.hs
@@ -1,59 +1,29 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
-{-# LANGUAGE TemplateHaskell   #-}
-{-# LANGUAGE DeriveGeneric #-}
 
 module Main where
 
-------------------------------------------------------------------------------
-import           Control.Applicative
-import           Control.Monad.Trans
-import           Control.Monad.Reader
-import           Control.Monad.State
-import           Data.ByteString (ByteString)
-import           Control.Lens
-import           Data.Maybe
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as T
-import           Data.Aeson.Types (ToJSON)
-import qualified Data.ByteString.Char8 as BS
-import           Data.Time.Clock
+import           Data.Aeson
+import           Data.Time.Calendar
 import           GHC.Generics
-import           Snap
-import           Snap.Snaplet.Auth
-import           Snap.Snaplet.Heist
-import           Snap.Snaplet.Session
-import           Snap.Snaplet.Session.Backends.CookieSession
-import           Snap.Extras.JSON (writeJSON)
-import           Snap.Util.FileServe
-import           Heist
-import           Text.XmlHtml hiding (render)
+import           Network.Wai
+import qualified Network.Wai.Handler.Warp as N
+import           Servant
 
+type API = "static" :> Raw
 
-------------------------------------------------------------------------------
-data App = App { _sess :: Snaplet SessionManager }
+api :: Proxy API
+api = Proxy
 
-makeLenses ''App
+server :: Server API
+server = serveDirectory "static"
 
-------------------------------------------------------------------------------
--- | The application's routes.
-routes :: [(ByteString, Handler App App ())]
-routes = [ ("/", method GET $ serveFile "frontend/boustro.html")
-         , ("elm.js", method GET $ serveFile "frontend/elm.js")
-         , ("style.css", method GET $ serveFile "frontend/style.css")
-         , ("texts", method GET $ serveDirectory "texts")
-         ]
-
-------------------------------------------------------------------------------
--- | The application initializer.
-app :: SnapletInit App App
-app = makeSnaplet "app" "An snaplet example application." Nothing $ do
-    s <- nestSnaplet "" sess $
-         initCookieSessionManager "site_key.txt" "_cookie" Nothing
-    addRoutes routes
-    return $ App s
-
+app :: Application
+app = serve api server
 
 main :: IO ()
-main = serveSnaplet defaultConfig app
+main = N.run 8000 app
